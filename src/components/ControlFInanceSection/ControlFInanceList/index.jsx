00b74e1e1ea2e40ec5f01713c842351d6a1c5334
bervalim <bernardogvalim@gmail.com>
2023-07-28 11:37:20 -0300

    nuKenzie

--- src/components/ControlFInanceSection/ControlFInanceList/index.jsx
+++ src/components/ControlFInanceSection/ControlFInanceList/index.jsx
@@ -1,28 +1,30 @@
 import { ControlFinanceCard } from "./ControlFinanceCard";
-
+import styles from "./style.module.scss";
 export const ControlFinanceList = ({
   financeList,
   removeFinanceControlCard,
 }) => {
   return (
     <section style={{ gridArea: "c" }}>
-      <h3 className="title two">Resumo Financeiro</h3>
-      {financeList.length > 0 ? (
-        <ul>
-          {financeList.map((finance) => (
-            <ControlFinanceCard
-              key={finance.id}
-              id={finance.id}
-              financialDescription={finance.financialDescription}
-              typeValue={finance.typeValue}
-              financialValue={finance.financialValue}
-              removeFinanceControlCard={removeFinanceControlCard}
-            />
-          ))}
-        </ul>
-      ) : (
-        <h2 className="title one">Você ainda não possui nenhum lançamento</h2>
-      )}
+      <div className={styles.ControlFinanceList}>
+        <h3 className="title two">Resumo Financeiro</h3>
+        {financeList.length > 0 ? (
+          <ul>
+            {financeList.map((finance) => (
+              <ControlFinanceCard
+                key={finance.id}
+                id={finance.id}
+                financialDescription={finance.financialDescription}
+                typeValue={finance.typeValue}
+                financialValue={finance.financialValue}
+                removeFinanceControlCard={removeFinanceControlCard}
+              />
+            ))}
+          </ul>
+        ) : (
+          <h2 className="title one">Você ainda não possui nenhum lançamento</h2>
+        )}
+      </div>
     </section>
   );
 };
